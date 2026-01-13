510b3060a6d2ad28d94ad9e174e7a23a29457a65
Juan Delgado <dsikorp@gmail.com>
2026-01-13 16:56:27 -0500

    Hooks personalizados fin

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -2,38 +2,10 @@ import { PreviousSearches } from './gifs/components/PreviousSearches';
 import { CustomHeader } from './shared/components/CustomHeader'
 import { GifList } from './gifs/components/GifList'
 import { SearchBar } from './shared/components/SearchBar'
-import { useState } from 'react';
-import { getGifsByQuery } from './gifs/actions/get-gifs-by-query.actions';
-import type { Gif } from './gifs/interfaces/gif.interface';
+import { useGifs } from './gifs/hooks/useGifs';
 
 export const GifsApp = () => {
-
-    const [gifs, setGifs] = useState<Gif[]>([]);
-    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-
-    const handleTermClicked = (term: string) => {
-        console.log(`${term}`);
-    };
-
-    const handleSearch = async (query: string = '') => {
-        console.log({ query });
-        query = query.trim().toLocaleLowerCase();
-
-        if (query.length === 0) {
-            return;
-        }
-
-        if (previousTerms.includes(query)) {
-            return;
-        }
-
-        setPreviousTerms([query, ...previousTerms].slice(0, 8));
-        const gifs = await getGifsByQuery(query);
-
-        console.log({ gifs });
-        setGifs(gifs);
-
-    };
+    const { previousTerms, handleSearch, handleTermClicked, gifs } = useGifs();
 
     return (
         <>
--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import { useRef, useState } from 'react'
+import { getGifsByQuery } from '../actions/get-gifs-by-query.actions';
+import type { Gif } from '../interfaces/gif.interface';
+
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
-    return (
-        <div>useGifs</div>
-    )
+    const [gifs, setGifs] = useState<Gif[]>([]);
+    const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+
+    const gifsCache = useRef<Record<string, Gif[]>>({});
+
+    const handleTermClicked = async (term: string) => {
+        console.log(`${term}`);
+
+        if (gifsCache.current[term]) {
+            setGifs(gifsCache.current[term]);
+            return;
+        }
+
+        const gifs = await getGifsByQuery(term);
+        setGifs(gifs);
+    };
+
+    const handleSearch = async (query: string = '') => {
+        console.log({ query });
+        query = query.trim().toLocaleLowerCase();
+
+        if (query.length === 0) {
+            return;
+        }
+
+        if (previousTerms.includes(query)) {
+            return;
+        }
+
+        setPreviousTerms([query, ...previousTerms].slice(0, 8));
+        const gifs = await getGifsByQuery(query);
+
+        console.log({ gifs });
+        setGifs(gifs);
+
+        gifsCache.current[query] = gifs;
+        console.log(gifsCache);
+
+    };
+
+
+    return {
+        // props
+        gifs,
+        // methods
+        handleSearch,
+        handleTermClicked,
+        previousTerms,
+    }
 }
